d2a54237cf2e1fa40a927f02d30008c750b591c0
gisselevelin23-blip <gisselevelin23@gmail.com>
2026-02-08 21:21:16 -0600

    Cambio realizado el día de hoy 08/02

--- app/src/main/java/com/ucenm/ucenm20261p/MainActivity.java
+++ app/src/main/java/com/ucenm/ucenm20261p/MainActivity.java
@@ -1,6 +1,10 @@
 package com.ucenm.ucenm20261p;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText nombres, apellidos;
+    Button btnagregar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        nombres = (EditText) findViewById(R.id.nombres);
+        apellidos = (EditText) findViewById(R.id.apellidos);
+        btnagregar = (Button) findViewById(R.id.btnagregar);
+
+        btnagregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),
+                        "Programacion UCENM " + nombres.getText() + " " + apellidos.getText(),
+                        Toast.LENGTH_LONG). show();
+            }
         });
+
+
     }
 }
